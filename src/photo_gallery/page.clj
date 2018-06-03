0229521eafe8c04f1c67d8c4f413a519eab4a05b
schmudde <d@schmud.de>
2018-06-03 17:22:30 -0400

    gallery display improvements

--- src/photo_gallery/page.clj
+++ src/photo_gallery/page.clj
@@ -2,16 +2,29 @@
   (:require [hiccup.core :as html]
             [hiccup.page :as html-page]))
 
-(def images (clojure.java.io/file "resources/img"))
+(defn valid-extension? [path]
+    (let [valid-exts [".gif" ".png" ".jpg"]
+          filename (.getName path)]
+      (some true? (-> filename
+                      (clojure.string/lower-case)
+                      (as-> lowercase-name (map #(clojure.string/includes? lowercase-name %) valid-exts))))))
 
-(defn img-display [link]
-  [:img {:src link :width "25%" :height "25%" :id (name link) :class "thumbnail"}])
+(def image-files (clojure.java.io/file "resources/img"))
 
-(defn group-display [name]
-  [:h1 (last (re-find  #"(resources/img/)([A-Za-z\-]+)" name))])
+(defn create-img-tag [img]
+  (if (valid-extension? img)
+    [:img {:src (.getPath img) :width "25%" :height "25%" :id (name (.getPath img)) :class "thumbnail"}]))
 
-(def images (mapv (fn [img]
-                    (if (.isFile img) (img-display (.getPath img)) (group-display (.getPath img)))) (file-seq images)))
+(defn get-trip-name [name]
+  [:h1 (last (re-find  #"(resources/img/)([A-Za-z\-]+)" (.getPath name)))])
+
+;; (.isFile (first (file-seq image-files))) => false
+;; (.isFile (nth (file-seq image-files) 2)) => true
+(def images (mapv (fn [file-object]
+                    (if (.isFile file-object)
+                      (create-img-tag file-object)
+                      (get-trip-name file-object))) (file-seq image-files)))
+
 (def button [:button {:id "modal-button"} "Open Modal"])
 (def modal [:div {:id "image-modal" :class "modal"}
             [:div {:class "modal-content"}
