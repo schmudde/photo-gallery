c201784c90e1e75cdb2b83579acca414474e8fc4
schmudde <d@schmud.de>
2018-06-05 15:08:01 -0400

    Thumbnail maker

--- src/photo_gallery/page.clj
+++ src/photo_gallery/page.clj
@@ -1,19 +1,24 @@
 (ns photo-gallery.page
   (:require [hiccup.core :as html]
-            [hiccup.page :as html-page]))
+            [hiccup.page :as html-page]
+            [image-resizer.resize :refer :all]
+            [image-resizer.format :as format]))
 
-(defn valid-extension? [path]
-    (let [valid-exts [".gif" ".png" ".jpg"]
-          filename (.getName path)]
+(defn valid-extension? [filename]
+    (let [valid-exts [".gif" ".png" ".jpg"]]
       (some true? (-> filename
                       (clojure.string/lower-case)
                       (as-> lowercase-name (map #(clojure.string/includes? lowercase-name %) valid-exts))))))
 
 (def image-files (clojure.java.io/file "resources/img"))
 
-(defn create-img-tag [img]
-  (if (valid-extension? img)
-    [:img {:src (.getPath img) :width "25%" :height "25%" :id (name (.getPath img)) :class "thumbnail"}]))
+(defn create-thumb-and-tag [img]
+  (let [filename (.getName img)
+        filepath (.getPath img)]
+    (if (valid-extension? filename)
+      (->> (str "resources/img/thumbnails/" filename)
+           (format/as-file ((resize-fn 400 400) img))
+           ((fn [thumbpath] [:img {:src thumbpath :width "25%" :height "25%" :id (name filepath) :class "thumbnail"}]))))))
 
 (defn get-trip-name [name]
   [:h1 (last (re-find  #"(resources/img/)([A-Za-z\-]+)" (.getPath name)))])
@@ -22,7 +27,7 @@
 ;; (.isFile (nth (file-seq image-files) 2)) => true
 (def images (mapv (fn [file-object]
                     (if (.isFile file-object)
-                      (create-img-tag file-object)
+                      (create-thumb-and-tag file-object)
                       (get-trip-name file-object))) (file-seq image-files)))
 
 (def button [:button {:id "modal-button"} "Open Modal"])
